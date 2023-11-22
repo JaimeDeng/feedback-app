94018697dcda0c0a3414cecb76540a8ab199b5f1
Jaime <jaime.deng@imsoft.com.tw>
2023-11-22 11:44:41 +0800

    components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Header from "./components/Header"
+
 function App() {
     return (
-        <h1>Hello from the app component</h1>
+        <>
+            <Header text='Hello World' />
+            <h1>Hello from the app component</h1>
+        </>
     )
 }
 
